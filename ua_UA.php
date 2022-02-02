f54199188d4eea4117c2effe68eacd667fe3d5cf
Sergei1213 <89149613+Sergei1213@users.noreply.github.com>
2022-02-02 15:02:15 +0200

    Add files via upload

---
+++ ua_UA.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * @version $Id: HEADER 1 2010-02-24 00:12 Tsmr $
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2010 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+// ----------------------------------------------------------------------
+// Original Author of file: CAILLAUD Xavier
+// Purpose of file: plugin validation v1.1.0 - GLPI 0.78
+// ----------------------------------------------------------------------
+ */
+
+$title = "Validation";
+
+$LANG['plugin_validation']['title'][1] = $title;
+
+$LANG['plugin_validation']["profile"][0] = "Manage rights";
+$LANG['plugin_validation']["profile"][1] = "Access rights";
+
+$LANG['plugin_validation']['migration'][0] = "Validations Migration Process into GLPI core";
+$LANG['plugin_validation']['migration'][1] = "WARNING : Unactivate notifications for migrate datas.";
+$LANG['plugin_validation']['migration'][3] = "Launch migration";
+$LANG['plugin_validation']['migration'][4] = "Nothing to migrate, you can uninstall the plugin";
+
+$LANG['plugin_validation']['valid'][1] = "Status";
+$LANG['plugin_validation']['valid'][3] = "Date";
+$LANG['plugin_validation']['valid'][4] = "Validation date";
+$LANG['plugin_validation']['valid'][5] = "Comment";
+$LANG['plugin_validation']['valid'][6] = "Validation comment";
+$LANG['plugin_validation']['valid'][15] = "Author";
+$LANG['plugin_validation']['valid'][31] = "Cancelled";
+
+?>
